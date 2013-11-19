913c548a3209b75164154694adb6f21234d7b535
Jill-J?nn Vie <vie@jill-jenn.net>
2013-11-19 18:28:43 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+Il faut d'abord installer [virtualenv](https://pypi.python.org/pypi/virtualenv) pour Python 2.7.
+
+Puis gfortran dans [ce humble bundle](http://www.scipy.org/scipylib/building/macosx.html#compilers-c-c-fortran-cython).
+
+    $ virtualenv .
+    $ pip install scipy
+    $ pip install numpy
--- guess.py
+++ guess.py
@@ -9,12 +9,12 @@ def entropy1(x):
 	return entropy([x, 1 - x])
 
 nb_questions = 10
-nb_answers = 5
+nb_objects = 5
 
-Q = [[[random.randint(1, 10) for _ in range(2)] for _ in range(nb_answers)] for _ in range(nb_questions)]
-Qt = [[float(Q[i][j][1]) / (Q[i][j][0] + Q[i][j][1]) for j in range(nb_answers)] for i in range(nb_questions)]
+Q = [[[random.randint(1, 10) for _ in range(2)] for _ in range(nb_objects)] for _ in range(nb_questions)] # Q[i][j][0-1] pour les valeurs α-β de la question j sachant que l'objet est i
+Qt = [[float(Q[i][j][1]) / (Q[i][j][0] + Q[i][j][1]) for j in range(nb_objects)] for i in range(nb_questions)] # Q[i][j] vaut l'espérance de la variable, c'est-à-dire p(Q_j = 1|X = i)
 
-p = [random.random() for _ in range(nb_answers)]
-p = [x / sum(p) for x in p]
+p = [random.random() for _ in range(nb_objects)]
+p = [x / sum(p) for x in p] # A priori
 
-print map(entropy1, np.squeeze(np.asarray(np.matrix(Qt).dot(p)))) - np.matrix([map(entropy1, q) for q in Qt]).dot(p) # H(E(Q)) - E(H(Q))
+print map(entropy1, np.squeeze(np.asarray(np.matrix(Qt).dot(p)))) - np.matrix([map(entropy1, q) for q in Qt]).dot(p) # H(E(Q_j)) - E(H(Q_j)) pour chaque Q_j
