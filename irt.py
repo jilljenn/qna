5b2f6f902c6bd4834b8b60305ef27e04c854235d
Jill-J?nn Vie <vie@jill-jenn.net>
2014-06-17 00:57:58 +0200

    Fix IRT

--- irt.py
+++ irt.py
@@ -14,13 +14,14 @@ class IRT():
 
 	def training_step(self, train, opt_Q=True, opt_sg=True):
 		nb_students = len(train)
+		nb_questions = len(train[0])
 		raw_data = map(int, reduce(lambda x, y: x + y, train))
 		a = r.matrix(robjects.IntVector(raw_data), nrow=nb_students, byrow=True)
 		model = ltm.rasch(a)
 		coeff = ltm.coef_rasch(model)
 		scores = ltm.factor_scores(model).rx('score.dat')[0].rx('z1')[0]
 		r('coeff <- coef(rasch(%s))' % a.r_repr())
-		r('one <- rep(1, 100)')
+		r('one <- rep(1, %d)' % nb_questions)
 		r('itembank <- cbind(coeff[,2:1], 1 - one, one)')
 
 	def load(self, filename):
@@ -30,10 +31,12 @@ class IRT():
 		r('theta <- 0')
 
 	def next_item(self, replied_so_far, results_so_far):
-		return r('nextItem(itembank, NULL, theta, out = c({}))$item'.format(','.join(map(str, replied_so_far))))[0]
+		next = r('nextItem(itembank, NULL, theta, out = c({}))$item'.format(','.join(map(lambda x: str(x + 1), replied_so_far))))[0]
+		return next - 1
 
 	def estimate_parameters(self, replied_so_far, results_so_far):
-		theta = r('theta <- thetaEst(itembank[c({}),], c({}))'.format(','.join(map(str, replied_so_far)), ','.join(map(str, results_so_far))))
+		scores_so_far = map(int, results_so_far)
+		theta = r('theta <- thetaEst(itembank[c({}),], c({}))'.format(','.join(map(lambda x: str(x + 1), replied_so_far)), ','.join(map(str, scores_so_far))))
 		pm = r('semTheta(theta, itembank[c({}),])'.format(','.join(map(str, replied_so_far))))
 
 	def predict_performance(self):
