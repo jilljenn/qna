8fafe51fb661a2346127bfa57d04958e04008ec7
Jill-J?nn Vie <vie@jill-jenn.net>
2015-02-09 22:49:24 +0100

    First last version

--- subset.py
+++ subset.py
@@ -3,6 +3,6 @@ import random
 
 with open('subset.json', 'w') as f:
     f.write(json.dumps({
-        'question_subset': range(20), # sorted(random.sample(range(40), 40)),
-        'train_subset': sorted(random.sample(range(536), 436)) # sorted(random.sample(range(296), 216))
+        'question_subset': range(17), # sorted(random.sample(range(40), 40)),
+        'train_subset': sorted(random.sample(range(58939), 48939)) # 536 436 sorted(random.sample(range(296), 216))
     }))
