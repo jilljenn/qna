0b1efe5ac72d3b703eb2f0b8cc494859e4d509b1
Jill-J?nn Vie <vie@jill-jenn.net>
2016-08-22 23:05:09 +0200

    Add cold start simulation

---
+++ coldstart.py
@@ -0,0 +1,102 @@
+# coding=utf-8
+from mirt import MIRT
+from qmatrix import QMatrix
+from calc import logloss, avgstd
+from conf import dataset_name, BATCH_SIZE
+from my_io import IO, Dataset, say
+import random
+import numpy as np
+
+
+def full_logloss(performance, truth):
+    return logloss(performance, truth, range(len(performance)))
+
+def hinge(performance, truth):
+    nb_questions = len(performance)
+    return sum(np.round(performance)[i] == truth[i] for i in range(nb_questions))
+
+
+def simulate(train_data, test_data):
+    q = QMatrix()
+    q.load('qmatrix-%s' % dataset_name)
+    # model = MIRT(q=q)
+    model = MIRT(dim=8)
+    model.training_step(train_data)
+    nb_students = len(test_data)
+    nb_questions = len(test_data[0])
+    error_log = {'random': [], 'dpp': [], 'cat': []}
+    chosen_once = random.sample(range(nb_questions), BATCH_SIZE)
+    # error_rate = [[] for _ in range(budget)]
+    for student_id in range(nb_students):
+        truth = np.array(test_data[student_id])
+        if student_id % 10 == 0:
+            print(student_id)
+        say('Étudiant', student_id, test_data[student_id])
+
+        # Random
+        model.init_test()
+        chosen = chosen_once  # random.sample(range(nb_questions), BATCH_SIZE)
+        # print('Random', chosen)
+        answers = truth[chosen]
+        model.bootstrap(chosen, answers)
+        performance = model.predict_performance()
+        error_log['random'].append(full_logloss(performance, truth))
+
+        say(full_logloss(performance, truth))
+        say(hinge(performance, truth), 'correct out of', nb_questions)
+
+        # DPP
+        model.init_test()
+        chosen = model.select_batch(BATCH_SIZE)
+        # print('DPP', chosen)
+        answers = np.array(test_data[student_id])[chosen]
+        model.bootstrap(chosen, answers)
+        performance = model.predict_performance()
+        error_log['dpp'].append(full_logloss(performance, truth))
+
+        say(full_logloss(performance, truth))
+        say(hinge(performance, truth), 'correct out of', nb_questions)
+
+        # CAT
+        model.init_test()
+        budget = BATCH_SIZE
+        replied_so_far = []
+        results_so_far = []
+        for t in range(1, budget + 1):
+            question_id = model.next_item(replied_so_far, results_so_far)
+            # say('\nRound', t, '-> We ask question', question_id + 1, 'to the examinee.')
+            # say('Correct!' if test_data[student_id][question_id] else 'Incorrect.') #, "I expected: %f." % round(
+            replied_so_far.append(question_id)
+            results_so_far.append(test_data[student_id][question_id])
+            model.estimate_parameters(replied_so_far, results_so_far)
+            performance = model.predict_performance()
+
+            # say(' '.join(map(lambda x: str(int(10 * round(x, 1))), performance)))
+            # say('Estimate:', ''.join(map(lambda x: '%d' % int(round(x)), performance)))
+            # say('   Truth:', ''.join(map(lambda x: '%d' % int(x), test_data[student_id])))
+            # say(full_logloss(performance, truth))
+        error_log['cat'].append(full_logloss(performance, truth))
+
+        say(full_logloss(performance, truth))
+        say(hinge(performance, truth), 'correct out of', nb_questions)
+
+
+    """for strategy in error_log:
+        print(error_log[strategy])"""
+    for strategy in ['random', 'dpp', 'cat']:
+        if len(error_log[strategy]) > 0:
+            print(avgstd(error_log[strategy]))
+
+
+def main():
+    files = IO()
+    dataset = Dataset(dataset_name, files)
+    dataset.load_subset()
+    print(dataset)
+    train_dataset = [dataset.data[i] for i in dataset.train_subset]
+    test_dataset = [dataset.data[i] for i in dataset.test_subset]
+    simulate(train_dataset, test_dataset)
+
+
+if __name__ == '__main__':
+    main()
--- mirt.py
+++ mirt.py
@@ -1,9 +1,11 @@
 # coding=utf8
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
-from calc import logloss, compute_mean_entropy
+from calc import logloss, compute_mean_entropy, sample_k
 from my_io import say, Dataset
+import numpy as np
 import random
+import os
 
 r = robjects.r
 cdm = importr('CDM')
@@ -18,40 +20,48 @@ class MIRT:
         self.name = 'MIRT'
         self.criterion = 'MFI'
         self.nb_questions = None
-        self.validation_question_set = None
+        self.validation_question_set = set()
         if q:
             self.q = q
             robjects.globalenv['entries'] = robjects.IntVector(q.get_entries())
             r("Q <- matrix(c(entries), ncol=%d, byrow=TRUE)" % self.dim)  # dimnames=list(NULL, c('F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8'))
 
     def training_step(self, train, opt_Q=True, opt_sg=True):
-        nb_students = len(train)
         self.nb_questions = len(train[0])
-        raw_data = map(int, reduce(lambda x, y: x + y, train))
-        data = r.matrix(robjects.IntVector(raw_data), nrow=nb_students, byrow=True)
-        robjects.globalenv['data'] = data
-        if self.q:
-            r('model = mirt.model(Q)')
-            r('fit = mirt(data, model)')
+        if os.path.isfile(self.get_prefix() + '.rdata'):
+            r.load(self.get_prefix() + '.rdata')
         else:
-            r('fit = mirt(data, %d)' % self.dim)
-        r('V <- coef(fit, simplify=TRUE)$items[,1:%d]' % (self.dim + 1))
-        print(r.V)
-        r("U <- cbind(fscores(fit, method='MAP', full.scores=TRUE), rep(1))")
-        print(r('U[1:5,]'))
-        r('Z <- U %*% t(V)')
-        r('p <- 1 / (1 + exp(-Z))')
-        print(r('Z[1:5,]'))
-        print(r('p[1:5,]'))
-        print(r('data[1:5,]'))
-        for line in Dataset('banach').data[:5]:
-            print(line)
+            nb_students = len(train)
+            raw_data = map(int, reduce(lambda x, y: x + y, train))
+            data = r.matrix(robjects.IntVector(raw_data), nrow=nb_students, byrow=True)
+            robjects.globalenv['data'] = data
+            if self.q:
+                r('model = mirt.model(Q)')
+                r('fit = mirt(data, model, method="MHRM")')
+            elif self.dim < 3:
+                r('fit = mirt(data, %d)' % self.dim)
+            else:
+                r('fit = mirt(data, %d, method="MHRM")' % self.dim)
+            r('V <- coef(fit, simplify=TRUE)$items[,1:%d]' % (self.dim + 1))
+            print(r.V)
+            r("U <- cbind(fscores(fit, method='MAP', full.scores=TRUE), rep(1))")
+            r('save(fit, U, V, data, file="%s.rdata")' % self.get_prefix())
+
+            """print(r('U[1:5,]'))
+            r('Z <- U %*% t(V)')
+            r('p <- 1 / (1 + exp(-Z))')
+            print(r('Z[1:5,]'))
+            print(r('p[1:5,]'))
+            print(r('data[1:5,]'))
+            for line in Dataset('banach').data[:5]:
+                print(line)"""
+        self.similarity = np.array(r.V.dot(r.V.transpose()))
 
     def load(self, filename):
         pass
 
-    def init_test(self, validation_question_set):
-        self.validation_question_set = validation_question_set
+    def init_test(self, validation_question_set=[]):
+        self.validation_question_set = set(validation_question_set)
         r("CATdesign <- mirtCAT(NULL, fit, criteria='Drule', start_item='Drule', local_pattern=data, design_elements=TRUE)")
 
     def next_item(self, replied_so_far, results_so_far):
@@ -70,15 +80,27 @@ class MIRT:
         r('CATdesign <- updateDesign(CATdesign, items=c(%s), response=c(%s))' % (replied_so_far[-1] + 1, int(results_so_far[-1])))
         r('CATdesign$person$Update.thetas(CATdesign$design, CATdesign$test)')
 
-        say('Thêta du candidat :', r('CATdesign$person$thetas')[0])
+        say('Thêta du candidat :', r('CATdesign$person$thetas'))
         # pm = r('semTheta(theta, itembank[c({}),])'.format(','.join(map(str, replied_so_far))))
+
+    def bootstrap(self, chosen, answers):
+        items_asked = ','.join(map(lambda x: str(x + 1), chosen))
+        answers_got = ','.join(map(lambda x: str(int(x)), answers))
+        r("CATdesign <- updateDesign(CATdesign, items=c(%s), response=c(%s))" % (items_asked, answers_got))
+        r("CATdesign$person$Update.thetas(CATdesign$design, CATdesign$test)")
+        r("theta <- cbind(CATdesign$person$thetas, 1)")
+        return r.theta
 
     def predict_performance(self, var_id=''):
         r('U <- cbind(CATdesign$person$thetas, rep(1))')
         r('Z <- U %*% t(V)')
         r('p <- 1 / (1 + exp(-Z))')
-        print(r.p)
+        # print(r.p)
         return tuple(r.p)
+
+    def select_batch(self, batch_size):
+        chosen = sample_k(range(r.V.dim[0]), self.similarity, batch_size)
+        return chosen
 
     def get_prefix(self):
         prefix = 'mirt'
