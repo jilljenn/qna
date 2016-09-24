c14eb4c198df348ae0411860393bfbe481b07831
Jill-Jênn Vie <vie@jill-jenn.net>
2016-09-24 07:25:52 +0200

    Automate plot generation

---
+++ all_plots.py
@@ -0,0 +1,11 @@
+import os.path
+import sys
+import glob
+
+folder = sys.argv[1]
+
+for path in glob.glob('%s/*' % folder):
+    if os.path.isdir(path) and not path.endswith('logs'):
+        for key in ['mean', 'count', 'delta']:
+            print('python plot.py %s %s' % (path, key))
+            os.system('python plot.py %s %s' % (path, key))
