5555fac1f2e9cd2ebde62aea88a4aa1eecf1af6a
Jill-Jênn Vie <jill-jenn.vie@riken.jp>
2018-01-12 10:45:15 +0900

    Split to train test for FM

--- for_fm.py
+++ for_fm.py
@@ -1,10 +1,17 @@
 from my_io import Dataset
+import random
+
 
 dataset = Dataset('berkeley')
 print(dataset.nb_students)
 print(dataset.nb_questions)
 #print(dataset.data)
-with open('/tmp/berkeley.dat', 'w') as f:
-	for i in range(dataset.nb_students):
-		for j in range(dataset.nb_questions):
-			f.write('::'.join([str(i + 1), str(j + 1), '1' if dataset.data[i][j] else '0', '1']) + '\n')
+with open('/tmp/train.dat', 'w') as train:
+	with open('/tmp/test.dat', 'w') as test:
+		for i in range(dataset.nb_students):
+			for j in range(dataset.nb_questions):
+				line = ','.join([str(i), str(j), '1' if dataset.data[i][j] else '0']) + '\n'
+				if random.random() < 0.8:
+					train.write(line)
+				else:
+					test.write(line)
