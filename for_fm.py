500d2a7edf7e96f90959c12ab5bb02c1db50bd38
Jill-Jênn Vie <jill-jenn.vie@riken.jp>
2018-02-15 15:17:56 +0900

    Generate datasets in new format

--- for_fm.py
+++ for_fm.py
@@ -1,17 +1,29 @@
 from my_io import Dataset
+import yaml
 import random
+import os
 
 
 dataset = Dataset('berkeley')
-print(dataset.nb_students)
-print(dataset.nb_questions)
-#print(dataset.data)
-with open('/tmp/train.dat', 'w') as train:
-	with open('/tmp/test.dat', 'w') as test:
-		for i in range(dataset.nb_students):
-			for j in range(dataset.nb_questions):
-				line = ','.join([str(i), str(j), '1' if dataset.data[i][j] else '0']) + '\n'
-				if random.random() < 0.8:
-					train.write(line)
-				else:
-					test.write(line)
+nb_train = 0
+with open('/tmp/train.csv', 'w') as train:
+    with open('/tmp/test.csv', 'w') as test:
+        for i in range(dataset.nb_students):
+            for j in range(dataset.nb_questions):
+                line = ','.join([str(i), str(j), '1' if dataset.data[i][j] else '0', '0', '0']) + '\n'
+                if random.random() < 0.8:
+                    nb_train += 1
+                    train.write(line)
+                else:
+                    test.write(line)
+
+os.system('cp /tmp/test.csv /tmp/val.csv')
+
+with open('/tmp/config.yml', 'w') as f:
+    config = {
+        'USER_NUM': dataset.nb_students,
+        'ITEM_NUM': dataset.nb_questions,
+        'NB_CLASSES': 2,
+        'BATCH_SIZE': nb_train
+    }
+    f.write(yaml.dump(config, default_flow_style=False))
