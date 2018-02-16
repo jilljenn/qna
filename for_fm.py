b9dbe29bedb626e5196b02e30ce1e2550e4be7d1
Jill-Jênn Vie <jill-jenn.vie@riken.jp>
2018-02-16 15:49:57 +0900

    Prepare q-matrices for training FMs

--- for_fm.py
+++ for_fm.py
@@ -1,25 +1,44 @@
 from my_io import Dataset
+from qmatrix import QMatrix
+from scipy.sparse import csr_matrix, save_npz
+import pandas as pd
 import yaml
 import random
-import os
+import os.path
 
 
-dataset = Dataset('berkeley')
+DATASET_NAME = 'fraction'
+DATA_FOLDER = '/Users/jilljenn/code/TF-recomm/data'
+CSV_FOLDER = os.path.join(DATA_FOLDER, DATASET_NAME)
+CSV_TRAIN = os.path.join(CSV_FOLDER, 'train.csv')
+CSV_TEST = os.path.join(CSV_FOLDER, 'test.csv')
+CSV_VAL = os.path.join(CSV_FOLDER, 'val.csv')
+CONFIG = os.path.join(CSV_FOLDER, 'config.yml')
+Q_NPZ = os.path.join(CSV_FOLDER, 'qmatrix.npz')
+
+
+dataset = Dataset(DATASET_NAME)
 nb_train = 0
-with open('/tmp/train.csv', 'w') as train:
-    with open('/tmp/test.csv', 'w') as test:
-        for i in range(dataset.nb_students):
-            for j in range(dataset.nb_questions):
-                line = ','.join([str(i), str(j), '1' if dataset.data[i][j] else '0', '0', '0']) + '\n'
-                if random.random() < 0.8:
-                    nb_train += 1
-                    train.write(line)
-                else:
-                    test.write(line)
+with open(CSV_TRAIN, 'w') as train:
+    with open(CSV_TEST, 'w') as test:
+        with open(CSV_VAL, 'w') as val:
+            for i in range(dataset.nb_students):
+                for j in range(dataset.nb_questions):
+                    line = ','.join([str(i), str(j), '1' if dataset.data[i][j] else '0', '0', '0']) + '\n'
+                    if random.random() < 0.8:
+                        nb_train += 1
+                        train.write(line)
+                    else:
+                        test.write(line)
+                        val.write(line)
 
-os.system('cp /tmp/test.csv /tmp/val.csv')
 
-with open('/tmp/config.yml', 'w') as f:
+qm = QMatrix()
+qm.load('qmatrix-%s' % DATASET_NAME)
+save_npz(Q_NPZ, csr_matrix(qm.Q))
+
+
+with open(CONFIG, 'w') as f:
     config = {
         'USER_NUM': dataset.nb_students,
         'ITEM_NUM': dataset.nb_questions,
